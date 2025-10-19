421bedfa2f766a5fc50d102dccf879a1afb5eea9
Davi Tavares Morais <davidemorais33@gmail.com>
2025-10-19 10:46:41 -0300

    f

--- agendar.php
+++ agendar.php
@@ -4,17 +4,13 @@ $usuario = "root";
 $senha = "";
 $banco = "salao";
 
-try {
-    // Conexão com o banco de dados usando PDO
-    $pdo = new PDO("mysql:host=$localhost;dbname=$banco", $usuario, $senha);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // Consulta à tabela correta
-    $sql = $pdo->query("SELECT * FROM salaomt");
+$conexao = new mysqli($localhost, $usuario, $senha, $banco);
 
-    echo "Número de registros: " . $sql->rowCount();
-
-} catch (PDOException $e) {
-    echo "Erro ao conectar ou consultar: " . $e->getMessage();
+if($conexao->connect_errno){
+     echo "erro";
+}
+else{
+    echo "centro";
 }
 ?>
--- salao.php
+++ salao.php
@@ -1,5 +1,10 @@
 <?php
-$nome = addcslashes($_POST['noma']);
-$tel = addcslashes($_POST['telefone']);
-$dh = addcslashes($_POST['data'])
+if(isset($_POST['submit'])){
+    print_($_POST['nome']);
+    print_($_POST['telefone']);
+    print_($_POST['data']);
+    print_($_POST['alergia']);
+     print_($_POST['servico']);
+
+}
 ?>

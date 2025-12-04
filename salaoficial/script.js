7b9e1c67be292305e761a40e2c1670ac36c54e30
Davi Tavares Morais <davidemorais33@gmail.com>
2025-12-03 21:21:05 -0300

    c

--- salaoficial/script.js
+++ salaoficial/script.js
@@ -16,3 +16,33 @@
     // adicionar active no botão clicado
     event.target.classList.add('active');
 }
+
+  const area = document.getElementById("servicos-area");
+    const mais = document.getElementById("fab");
+
+    mais.addEventListener("click", () => {
+
+        // cria box do serviço
+        const box = document.createElement("div");
+        box.classList.add("servico-box");
+
+        box.innerHTML = `
+            <label>Nome do Serviço:</label>
+            <input type="text" placeholder="Ex: Corte Feminino" />
+
+            <label>Preço:</label>
+            <input type="number" placeholder="Ex: R$ 45,00" />
+
+            <button class="delete-btn">Excluir</button>
+            <button class="enviar-btn">Enviar</button>
+        `;
+
+        // função excluir
+        box.querySelector(".delete-btn").addEventListener("click", () => {
+            box.remove();
+        });
+
+        // adiciona o card na área
+        area.appendChild(box);
+        
+    });

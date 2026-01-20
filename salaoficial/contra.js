ffb8b8a0c305c1a64b2536c75212547819176d3b
Davi Tavares Morais <davidemorais33@gmail.com>
2026-01-19 21:37:22 -0300

    d

--- salaoficial/contra.js
+++ salaoficial/contra.js
@@ -1,12 +1,25 @@
- const clientes = document.querySelector('.clientes');
+ 
+
+const clientes = document.querySelector('.clientes');
 const btnTroca = document.getElementById('btnTroca');
+const textoPainel = document.getElementById('textoPainel');
+const textsalao = document.getElementById('textsalao');
+const textcriar = document.getElementById('criar');
+
 
 function trocarPainel() {
     clientes.classList.toggle('ativo');
 
     if (clientes.classList.contains('ativo')) {
         btnTroca.textContent = 'Login';
+        textoPainel.textContent = 'crie sua conta!';
+       // textsalao.textContent = 'Um espaço feito para cuidar da beleza e do bem-estar da mulher.'
+       textsalao.innerHTML = "Um espaço feito para <br> cuidar da beleza e do <br> bem-estar da mulher"
+       textcriar.textContent = 'Já tem cadastro? Acesse sua conta'
     } else {
-        btnTroca.textContent = 'Cadastro';
+        btnTroca.textContent = 'Conta';
+        textoPainel.textContent = 'seja bem-vindo!';
+        textsalao.innerHTML = "Cuidando da beleza da<br>mulher com carinho e<br>excelência";
+        textcriar.textContent = 'Ainda não tem uma conta? Crie uma.'
     }
 }

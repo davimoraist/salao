ba09122b7d364b5718c3610f1fe41f648d07bd3b
Davi Tavares Morais <davidemorais33@gmail.com>
2026-01-26 13:40:54 -0300

    o

---
+++ teste/script.js
@@ -0,0 +1,11 @@
+ const cliente = document.querySelector('.cliente');
+const cadastro = document.querySelector('#conecte');
+const conecte = document.querySelector('#cadastro');
+
+cadastro.addEventListener('click', () => {
+    cliente.classList.add('activo');
+});
+
+conecte.addEventListener('click', () => {
+    cliente.classList.remove('activo');
+});

f7ec6e1dfb0943f739f19fab427ac3e59cb3bb46
Davi Tavares Morais <davidemorais33@gmail.com>
2026-02-23 23:16:28 -0300

    d

--- salaoficial/adm/adm.php
+++ salaoficial/adm/adm.php
@@ -23,12 +23,19 @@ try {
 }
 
 // Pega todos os clientes
-  $sql = $pdo->query("   SELECT  c.id, c.nome,  c.email, p.id_cliente, p.endereco, p.telefone, p.cpf, p.data_nascimento, p.como_conheceu,  p.data_cadastro,
+ $sql = $pdo->query("SELECT c.id, c.nome, c.email, p.endereco, p.telefone, p.cpf, p.data_nascimento, p.como_conheceu, p.data_cadastro, f.diabetes, f.gestante, f.alergias, f.especificar_alergia, f.cuticula, f.onicomicose, f.especificar_onico, f.medicamento, f.qual_medicamento, f.lamina, f.outro_lamina_texto, f.encravada, f.onicofagia, f.esporte, f.piscina, f.data_cadastro AS data_ficha 
 
-        f.diabetes, f.gestante, f.alergias, f.especificar_alergia, f.cuticula, f.onicomicose, f.especificar_onico, f.medicamento,f.qual_medicamento, f.lamina, f.outro_lamina_texto, f.encravada,f.onicofagia, f.esporte,f.piscina, f.data_cadastro AS data_ficha FROM cliente c INNER JOIN pessoais p  ON c.id = p.id_cliente LEFT JOIN ficha_anamnese f ON c.id = f.id_cliente
+FROM cliente c
+
+LEFT JOIN pessoais p 
+    ON c.id = p.id_cliente 
+
+LEFT JOIN ficha_anamnese f 
+    ON c.id = f.id_cliente
+
+ORDER BY c.id DESC
 ");
 $clientes = $sql->fetchAll(PDO::FETCH_ASSOC);
-
 ?>
 
 <!DOCTYPE html>
@@ -88,7 +95,7 @@ $clientes = $sql->fetchAll(PDO::FETCH_ASSOC);
         <th>Email</th>
         <th>Ver mais</th>
     </tr>
-
+    
     <?php foreach ($clientes as $index => $user): ?>
     <tr>
         <td><?= htmlspecialchars($user['nome']) ?></td>

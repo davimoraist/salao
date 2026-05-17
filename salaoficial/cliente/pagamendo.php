0456708a311954da3eaa6375f83e3fc797cb101b
Davi Tavares Morais <davidemorais33@gmail.com>
2026-05-16 21:29:43 -0300

    oi

--- salaoficial/cliente/pagamendo.php
+++ salaoficial/cliente/pagamendo.php
@@ -1,20 +1,41 @@
-<!DOCTYPE html>
+ <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>mtnaildesigner.com</title>
-    <link rel="stylesheet" href="css/pagamento.css">
 
+    <link rel="stylesheet" href="css/pagamento.css">
 </head>
 <body>
-     
-    <div class="paga">
-        <h1>seu angedamendo voi marcodo</h1>
 
-        
-         <input type="button" value="voltar" onclick="pagamento()">
-    </div>
-    <script src="pagamento.js"></script>
+<div class="paga">
+    <h1>Seu agendamento foi marcado</h1>
+    <?php
+    require_once "conecte.php";
+    session_start();
+    ?>
+
+    <table>
+        <tbody>
+            <td>serviço</td>
+            <td>serviço</td>
+        </tbody>
+        <tbody>
+            <td>preço</td>
+            <td>preço</td>
+        </tbody>
+            <tbody>
+                <td>Total</td>
+                <td>Total</td>
+            </tbody>
+    </table>
+
+<input type="button" value="Pagamento" onclick="pagamento()">
+
+</div>
+
+<script src="pagamento.js"></script>
+
 </body>
 </html>

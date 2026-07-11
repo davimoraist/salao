5e2d7dd9c8898beaaa25ddeaa7ca2043c28ed6d3
Davi Tavares Morais <davidemorais33@gmail.com>
2026-07-10 22:54:16 -0300

    oi\

---
+++ salaoficial/cliente/paga.js
@@ -0,0 +1,5 @@
+function pixgera(){
+    var areis = window.document.getElementsByClassName('forma-pagamendo')
+    var paga = window.document.getElementsByClassName('texto')
+    paga [0].innerHTML = `PAGA AGORA`
+}

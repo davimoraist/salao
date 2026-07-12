0bca4ad5e034aec79768305ea0b31b34a7f5e7fe
Davi Tavares Morais <davidemorais33@gmail.com>
2026-07-12 17:54:23 -0300

    oi

--- salaoficial/cliente/paga.js
+++ salaoficial/cliente/paga.js
@@ -1,5 +1,24 @@
-function pixgera(){
-    var areis = window.document.getElementsByClassName('forma-pagamendo')
-    var paga = window.document.getElementsByClassName('texto')
-    paga [0].innerHTML = `PAGA AGORA`
+// Função para esconder todas as formas de pagamento do lado direito
+function esconderTodosMetodos() {
+    document.getElementById('container-pix').style.display = 'none';
+    document.getElementById('container-credito').style.display = 'none';
+    document.getElementById('container-debito').style.display = 'none';
+}
+
+// Ativa a tela do PIX
+function pixgera() {
+    esconderTodosMetodos();
+    document.getElementById('container-pix').style.display = 'block';
+}
+
+// Ativa a tela de Crédito
+function creditopaga() {
+    esconderTodosMetodos();
+    document.getElementById('container-credito').style.display = 'block';
+}
+
+// Ativa a tela de Débito (Corrigido o nome de deditopaga para debitopaga)
+function debitopaga() {
+    esconderTodosMetodos();
+    document.getElementById('container-debito').style.display = 'block';
 }
